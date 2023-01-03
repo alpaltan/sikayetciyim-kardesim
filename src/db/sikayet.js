18ee22ef735fe1b9342e5a50ada687262b87d919
alpaltan <alpaltnceng@gmail.com>
2023-01-03 16:50:06 +0300

    feat2

--- src/db/sikayet.js
+++ src/db/sikayet.js
@@ -1,20 +1,17 @@
 
-import fs from "fs";
 
-var fonksiyon = () => {
+const fs = require("fs");
+fs.readFile("./veriler.json", "utf8", (err, jsonString) => {
+    if (err) {
+        console.log("Error reading file from disk:", err);
+        return;
+    }
+    try {
+        const customer = JSON.parse(jsonString);
+        console.log("Customer address is:", customer.sikayetler[0].Tarih);
+    } catch (err) {
+        console.log("Error parsing JSON string:", err);
+    }
+});
 
-    fs.readFileSync("./veriler.json", "utf8", (err, jsonString) => {
-        if (err) {
-            console.log("Error reading file from disk:", err);
-            return;
-        }
-        try {
-            const customer = JSON.parse(jsonString);
-            console.log("Customer address is:", customer.sikayetler[0].Tarih);
-        } catch (err) {
-            console.log("Error parsing JSON string:", err);
-        }
-    });
-}
 
-export default fonksiyon;
